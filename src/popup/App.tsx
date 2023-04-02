f2225f3eaf86551a7b1cb837a168933e3f5e6be6
bappadityabera <bappaditya.bera@tier5.in>
2023-04-02 22:09:33 +0530

    new update

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,36 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 type Props = {};
 
 const App = (props: Props) => {
-  const [onBreak,setOnBreak]=useState(false);
+  const [onBreak, setOnBreak] = useState(false);
+  const [time, setTime] = useState(new Date());
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
-  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  chrome.runtime.onMessage.addListener(function (
+    request,
+    sender,
+    sendResponse
+  ) {
     if (request.message === "Button clicked!") {
       // Do something when the button is clicked on the page
-      console.log("helooooooooo")
+      console.log("helooooooooo");
     }
   });
   const handleClick = () => {
     chrome.runtime.sendMessage({ action: "clickButton" }, function (response) {
       console.log(response);
-       if(response.status==="done"){
+      if (response.status === "done") {
         setOnBreak(!onBreak);
-       }
+      }
     });
   };
-  
-  const handleStop=()=>{
+
+  const handleStop = () => {
     chrome.runtime.sendMessage({ action: "stopBreak" }, function (response) {
-      if(response.status==="done"){
+      if (response.status === "done") {
         setOnBreak(!onBreak);
-       }
-      });
       }
+    });
+  };
   return (
     <div>
-      <button onClick={handleClick}>start Break</button>
-      <button onClick={handleStop}>stop Break</button>
+      <div className="btn_wraper">
+        <button onClick={handleClick} className="start_button">
+          {time.toLocaleTimeString()}
+          <br />
+          start Break
+        </button>
+        <button onClick={handleStop} className="stop_button">
+          {/* <svg viewBox="25 25 50 50" className="DN">
+            <circle cx="50" cy="50" r="20"></circle>
+          </svg> */}
+          stop Break
+        </button>
+      </div>
     </div>
   );
 };
--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./popup.css";
+import "./popup.scss";
 import App from "./App";
 
 const popup: React.FC<{}> = () => {
